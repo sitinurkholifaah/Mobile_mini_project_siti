f922f0dd7ee5a8cfb6091c2f9b6535f05b2ead01
sitinurkholifaah <nurkholifahsiti968@gmail.com>
2022-05-21 11:46:08 +0700

    Mobile mini project

---
+++ Test Suites/TS01 Mobile.ts
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS01 Mobile</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>55372824-639d-474f-b820-39b520e0e03b</testSuiteGuid>
+   <testCaseLink>
+      <guid>1c914b1a-db7f-4d88-b797-8e50e7402d53</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Register/TC02 Register Negative</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0bfba334-9c54-41e4-9ef5-7f3d92fe567c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Register/TC01 Register Positive</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7ff7b6bc-a10d-4d03-9d29-52d204ab5aa0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/TC04 Login Negative</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8be93b26-20f2-42df-9040-1f7607b30923</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/TC03 Login Positve</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>38bc7e24-fed6-4003-b406-0cd0e3c3a010</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Homepage/TC05 Add to Cart - Copy</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>83326c1b-d36a-4c55-b908-6ea6e8757b29</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Cart/TC06 View Cart - Copy</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>515b2903-1f6e-41b4-879c-01d974e7fd69</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Logout/TC07 Logout - Copy</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
